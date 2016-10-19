8393bebfe0674095a4eebe7877fb9847c22030ca
Brad Ellis <bradjellis18@gmail.com>
2016-10-19 21:09:30 +0000

    retrieving artist and related artists

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var events = require("events");
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
+    if (endpoint === 'artists') {
+       endpoint = 'artists/' + args + '/related-artists';
+    }
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
            .end(function(response) {
@@ -29,24 +32,35 @@ app.get('/search/:name', function(req,res) {
     
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        res.json(artist);
+        var relatedReq = getFromApi('artists', artist.id);
+    
+        relatedReq.on('end', function(item) {
+            artist.related = item.artists;
+            var artistLength = artist.related.length;
+            var checked = 0;
+            
+            var checkList = function() {
+                if (checked === artistLength) {
+                    res.json(artist);
+                }
+            };
+            
+            artist.related.forEach(function(){
+                checked += 1;
+                checkList();
+            });
+        });
+        
+        relatedReq.on('error', function(code) {
+            res.status(404);
+        });
     });
     
     searchReq.on('error', function(code) {
         res.sendStatus(code);
     });
     
-    var relatedReq = getFromApi('artists', {
-        id: ,
-    });
     
-    relatedReq.on('end', function(item) {
-        
-    });
-    
-    relatedReq.on('error', function(code) {
-        
-    })
 });
 
 app.listen(process.env.PORT || 8080);
